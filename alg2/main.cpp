7772490c0f1b6a9584bdcb823bfddd84e08331b2
Martin Hafis <hafis@protonmail.com>
2016-11-10 21:02:05 +0100

    ALG-75 added completed Game of Life (manual preset)

--- alg2/main.cpp
+++ alg2/main.cpp
@@ -1,34 +1,22 @@
 #include <iostream>
 
-const int N = 10;
-
-bool Colony[N][N] =
+bool Colony[6][6] =
 {
-	{true, true, false, false, true, true, true, true, false, false},
-	{true, true, true, false, true, false, true, true, true, true},
-	{true, true, true, false, true, true, true, false, true, true},
-	{true, false, true, false, true, true, true, true, true, true},
-	{true, true, true, false, true, false, true, false, false, true},
-	{true, true, true, true, true, true, false, true, true, true},
-	{false, true, true, true, true, true, false, true, false, true},
-	{true, true, false, false, true, true, true, true, false, true},
-	{false, true, true, true, true, false, true, true, true, true},
-	{false, true, true, true, true, true, true, true, true, false}
+	{false, false, false, false, false, false},
+	{false, false, false, true, false, false},
+	{false, true, false, false, true, false},
+	{false, true, false, false, true, false},
+	{false, false, true, false, false, false},
+	{false, false, false, false, false, false}
 };
 
-bool pattern1[3][4] = {
-{true, false, true, false},
-{true, false, true, false},
-{true, false, true, false}
-};
+bool Colony2[6][6] = {};
 
-// -UNUSED
-template <const int rows, const int cols> bool isAlive(const bool colony[rows][cols], int row, int col) {
+template <const int rows, const int cols> bool isAlive(bool colony[rows][cols], const int& row, const int& col) {
     return ((row >= 0 && row < rows && col >= 0 && col < cols) ? colony[row][col] : false);
 }
 
-
-template <const int rows, const int cols, const int pattern_rows, const int pattern_cols> unsigned int similarityIndex(const bool colony[rows][cols], const int& left_row, const int& left_col, bool pattern[pattern_rows][pattern_cols]) {
+template <const int rows, const int cols, const int pattern_rows, const int pattern_cols> unsigned int similarityIndex(bool colony[rows][cols], const int& left_row, const int& left_col, bool pattern[pattern_rows][pattern_cols]) {
     unsigned int similarity = 0;
     for (int x = 0; x < pattern_rows; x++) {
         for (int y = 0; y < pattern_cols; y++) {
@@ -38,7 +26,7 @@ template <const int rows, const int cols, const int pattern_rows, const int patt
     return similarity;
 }
 
-template <const int rows, const int cols, const int pattern_rows, const int pattern_cols> bool hasPattern(const bool colony[rows][cols], bool pattern[pattern_rows][pattern_cols], int& row, int& col) {
+template <const int rows, const int cols, const int pattern_rows, const int pattern_cols> bool hasPattern(bool colony[rows][cols], bool pattern[pattern_rows][pattern_cols], int& row, int& col) {
     for (row = 0; row < (rows - pattern_rows + 1); row++) {
         for (col = 0; col < (cols - pattern_cols + 1); col++) {
             if (similarityIndex<rows, cols, pattern_rows, pattern_cols>(colony, row, col, pattern) == pattern_rows * pattern_cols) {
@@ -51,28 +39,52 @@ template <const int rows, const int cols, const int pattern_rows, const int patt
     return false;
 }
 
-void PrintColony(const bool Colony[N][N])
-{
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < N; j++)
-		{
-		    std::cout << (Colony[i][j] ? "X" : ".");
-		}
-		std::cout << std::endl;
-	}
+template <const int rows, const int cols, const int area> unsigned int areInState(bool colony[rows][cols], const int& row, const int& col, bool outsideState) {
+    unsigned int inState = 0;
+    for (int x = row - 1; x < row - 1 + area; x++) {
+        for (int y = col - 1; y < col - 1 + area; y++) {
+            if (x == row && y == col) continue;
+            if (isAlive<rows, cols>(colony, x, y) == outsideState) inState++;
+        }
+    }
+    return inState;
+}
+
+template <const int rows, const int cols> void printColony(bool colony[rows][cols], std::ostream& stream) {
+    for (int x = 0; x < rows; x++) {
+        for (int y = 0; y < cols; y++) {
+            stream << (colony[x][y] ? "X" : ".");
+        }
+        stream << std::endl;
+    }
+}
+
+template <const int rows, const int cols> void copyColony(bool colony[rows][cols], bool newColony[rows][cols]) {
+    for (int x = 0; x < rows; x++) {
+        for (int y = 0; y < cols; y++) {
+            colony[x][y] = newColony[x][y];
+        }
+    }
+}
+
+template <const int rows, const int cols> void updateColony(bool colony[rows][cols], bool newColony[rows][cols]) {
+    unsigned int t = 0;
+    for (int x = 0; x < rows; x++) {
+        for (int y = 0; y < cols; y++) {
+            t = areInState<rows, cols, 3>(colony, x, y, true);
+            newColony[x][y] = (colony[x][y] ? ((t < 2 || t > 3) ? false : true) : (t == 3));
+        }
+    }
+    copyColony<rows, cols>(colony, newColony);
 }
 
 int main()
 {
-	int x = 0, y = 0;
-	PrintColony(Colony);
-	bool p1 = hasPattern<N, N, 3, 4>(Colony, pattern1, x, y);
-	if (p1) {
-        std::cout << "Pattern1 se vyskytuje na: " << x << " " << y << std::endl;
-	} else {
-        std::cout << "Pattern1 se nevyskytuje - vraceno: " << x << " " << y << std::endl;
-	}
+    char c;
+    do {
+        printColony<6, 6>(Colony, std::cout);
+        updateColony<6, 6>(Colony, Colony2);
+    } while ((c = std::cin.get()) != 'E');
 
 	return 0;
 }
